fe1847a5f0537a65bb78fa150f06047956ea4cd4
Mottttton <mottttton.webdev@gmail.com>
2024-01-22 17:02:02 +0900

    AjaxをjQueryに書き換え

--- javascripts/ajax_sample.js
+++ javascripts/ajax_sample.js
@@ -1,27 +1,23 @@
-let jsonIndex = 0;
-const titleArea = document.getElementById("title");
-const contentArea = document.getElementById("content");
-const videoArea = document.getElementById("video");
-const btn = document.getElementById("btn");
+$(function () {
+  const titleArea = $("#title");
+  const contentArea = $("#content");
+  const videoArea = $("#video");
+  const btn = $("#btn");
+  let jsonIndex = 0;
 
-function getData() {
-  btn.addEventListener("click", function () {
-    const request = new XMLHttpRequest();
-    request.onreadystatechange = function () {
-      if (request.readyState == 4) {
-        if (request.status == 200) {
-          titleArea.innerHTML = request.response[jsonIndex].title;
-          contentArea.innerHTML = request.response[jsonIndex].content;
-          videoArea.setAttribute("src", request.response[jsonIndex].url);
+  function getData() {
+    btn.click(function () {
+      $.ajax('ajax.json', {
+        type: 'GET',
+        success: function (data) {
+          titleArea.html(data[jsonIndex].title);
+          contentArea.html(data[jsonIndex].content);
+          videoArea.attr('src', data[jsonIndex].url);
           jsonIndex == 2 ? jsonIndex = 0 : jsonIndex++;
-        }
-      }
-    };
-    // リクエスト送信
-    request.open("GET", "ajax.json");
-    request.responseType = "json";
-    request.send(null);
-  });
-}
+        },
+      })
+    });
+  };
 
-window.onload = getData;
+  getData();
+})
