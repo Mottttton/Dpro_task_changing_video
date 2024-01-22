b9d6cffa7f97130d417e417a7eaa763020b92f2c
Mottttton <mottttton.webdev@gmail.com>
2024-01-23 07:09:36 +0900

    submit task: change video

--- javascripts/ajax_sample.js
+++ javascripts/ajax_sample.js
@@ -1,25 +1,44 @@
 let number = 0;
+let data = [];
 const videoArea = document.getElementById("video");
 const titleArea = document.getElementById("title");
 const contentArea = document.getElementById("content");
 const button = document.getElementById('btn');
 
 function getData() {
-  button.addEventListener('click', e => {
-    const request = new XMLHttpRequest();
-    request.onreadystatechange = function() {
-      if (request.readyState == 4) {
-        if(request.status == 200) {
-          titleArea.innerHTML = request.response[number].title;
-          contentArea.innerHTML = request.response[number].content;
-          videoArea.setAttribute("src", request.response[number].url);
-          number == 2 ? number = 0 : number++;
-        }
+  const d = new $.Deferred;
+  const request = new XMLHttpRequest();
+  request.onreadystatechange = function() {
+    if (request.readyState == 4) {
+      if(request.status == 200) {
+        d.resolve(request);
       }
     }
-    request.open("GET", "ajax.json");
-    request.responseType = "json";
-    request.send(null);
+  }
+  request.open("GET", "ajax.json");
+  request.responseType = "json";
+  request.send(null);
+  return d.promise();
+}
+
+function setVideo(element) {
+  titleArea.innerHTML = element.response[number].title;
+  contentArea.innerHTML = element.response[number].content;
+  videoArea.setAttribute("src", element.response[number].url);
+  number == 2 ? number = 0 : number++;
+}
+
+function changeVideo() {
+  button.addEventListener('click', e => {
+    if(data.length < 1){
+      getData().then(function(d) {
+        data = d;
+        setVideo(data);
+      });
+    } else {
+      setVideo(data);
+    }
   })
 }
-window.onload = getData;
+
+window.onload = changeVideo;
